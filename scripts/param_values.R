2cb43bf177f0c681816993891b41f17d84c46cb3
Nathan Green <n8thangreen@yahoo.co.uk>
2018-12-30 17:06:46 +0000

    spacing

--- scripts/param_values.R
+++ scripts/param_values.R
@@ -1,9 +1,9 @@
 
 branch_costs <-
   list(
-    "Contact tracing per contact"	= 368.9,
+    "Contact tracing per contact" = 368.9,
     "Mean number of contacts examined per primary case"	= 6.5,
-    "Total Contact tracing"	= 2397.85,
+    "Total Contact tracing" = 2397.85,
 
     "Cost of inpatient episode for acute TB" = 3325.15,
     "Proportion of patients with acute TB who are admitted"	= 0.53,
@@ -13,15 +13,15 @@ branch_costs <-
     "Culture tests per case" = 4,
     "CXR per case" = 2,
     "LFT per case" = 4,
-    "Total Cost of tests"	= 104.74,
+    "Total Cost of tests" = 104.74,
 
     "HRZE tablet (60)" = 39.5,
     "_tablets per month"	= 150,
     "HRZE per month" = 98.75,
     "HR tablet 300/150 (56)" = 25.22,
-    "tablets per month" =	60,
+    "tablets per month" = 60,
     "HR per month" = 27.02142857,
-    "Duration of initial therapy"	= 2,
+    "Duration of initial therapy" = 2,
     "Duration of continious therapy" = 4,
     "Total Cost of chemotherapy" = 305.5857143,
 
@@ -31,17 +31,17 @@ branch_costs <-
     "Visits from TB nurse per case" = 6,
     "Total Out patient care" = 755.86,
 
-    "TST" =	18.62,
-    "QFT"	= 23.65,
+    "TST" = 18.62,
+    "QFT" = 23.65,
     "TB special nurse visit" = 44.31,
     "T-SPOT" = 35.12,
 
     "Out patient consultation (first visit)" = 208,
-    "CXR"	= 30.21,
+    "CXR" = 30.21,
     "LFT" = 3.02,
     "Number of outpatient consultation" = 1,
     "Number of CXR" = 1,
-    "Number of LFT"	= 1,
+    "Number of LFT" = 1,
     "Total Cost of positive screening" = 241.23,
 
     "Follow-up via nurses" = 44.31, #22.15, 66.46,
@@ -69,87 +69,87 @@ SA_params <-
     ),
     QFT_sens = list(
       distn = "pert",
-      params = c(min = 0.8,	mode = 0.77,	max = 0.84)
+      params = c(min = 0.8, mode = 0.77, max = 0.84)
     ),
     QFT_spec = list(
       distn = "pert",
-      params = c(mode = 0.97,	min = 0.94,	max = 0.99)
+      params = c(mode = 0.97, min = 0.94, max = 0.99)
     ),
     TSPOT_sens = list(
       distn = "pert",
-      params = c(mode = 0.9,	min = 0.87,	max = 0.93)
+      params = c(mode = 0.9, min = 0.87, max = 0.93)
     ),
     TSPOT_spec = list(
       distn = "pert",
-      params = c(mode = 0.95,	min = 0.92,	max = 0.98)
+      params = c(mode = 0.95, min = 0.92, max = 0.98)
     ),
     pAccept_chemo = list(
       distn = "pert",
-      params = c(mode = 0.95,	min = 0.5,	max = 1)
+      params = c(mode = 0.95, min = 0.5, max = 1)
     ),
     pComp_chemo = list(
       distn = "pert",
-      params = c(mode = 0.8,	min = 0.5, max = 	0.9)
+      params = c(mode = 0.8, min = 0.5, max =  0.9)
     ),
     pHep = list(
       distn = "pert",
-      params = c(mode = 0.002,	min = 0.001, max = 0.003)
+      params = c(mode = 0.002, min = 0.001, max = 0.003)
     ),
     Eff_comp = list(
       distn = "pert",
-      params = c(mode = 0.65,	min = 0.5, max = 0.8)
+      params = c(mode = 0.65, min = 0.5, max = 0.8)
     ),
     Eff_incomp = list(
       distn = "pert",
-      params = c(mode = 0.21,	min = 0.1, max = 0.3)
+      params = c(mode = 0.21, min = 0.1, max = 0.3)
     ),
     pTB = list(
       distn = "pert",
-      params = c(mode = 0.12,	min = 0.08,	max = 0.19)
+      params = c(mode = 0.12, min = 0.08,	max = 0.19)
     ),
     TST_cost = list(
       distn = "pert",
-      params = c(mode = 18.62,	min = 9.31,	max = 37.24)
+      params = c(mode = 18.62, min = 9.31, max = 37.24)
     ),
     QFT_cost = list(
       distn = "pert",
-      params = c(mode = 23.65,	min = 12.33, max = 45.29)
+      params = c(mode = 23.65, min = 12.33, max = 45.29)
     ),
     TSPOT_cost = list(
       distn = "pert",
-      params = c(mode = 35.12,	min = 18.06, max = 68.23)
+      params = c(mode = 35.12, min = 18.06, max = 68.23)
     ),
     CXR_cost = list(
       distn = "pert",
-      params = c(mode = 30.21,	min = 23.16, max = 35.25)
+      params = c(mode = 30.21, min = 23.16, max = 35.25)
     ),
     LFT_cost = list(
       distn = "pert",
-      params = c(mode = 3.02,	min = 2.01,	max = 4.03)
+      params = c(mode = 3.02, min = 2.01, max = 4.03)
     ),
     Ns_cost	= list(
       distn = "pert",
-      params = c(mode = 44.31,	min = 22.15, max = 66.46)
+      params = c(mode = 44.31, min = 22.15, max = 66.46)
     ),
     Hep_cost = list(
       distn = "pert",
-      params = c(mode = 732.13,	min = 366.06,	max = 1464.25)
+      params = c(mode = 732.13, min = 366.06, max = 1464.25)
     ),
     TB_cost	= list(
       distn = "pert",
-      params = c(mode = 4925.76,	min = 2462.88, max = 9851.52)
+      params = c(mode = 4925.76, min = 2462.88, max = 9851.52)
     )
   )
 
 branch_probs <-
   list(
-    "pAccept_TST"	= 0.982,
-    "pTSTread"	= 0.979,
+    "pAccept_TST" = 0.982,
+    "pTSTread" = 0.979,
     "pAccept_IGRA" = 0.992,
-    "pAccept_IGRA_TST+"	= 0.995,
-    "TSTIGRA_pos"	= 0.214,
-    "Dual_sens"	= 0.632,
-    "Dual_spec"	= 0.988,
+    "pAccept_IGRA_TST+" = 0.995,
+    "TSTIGRA_pos" = 0.214,
+    "Dual_sens" = 0.632,
+    "Dual_spec" = 0.988,
     "pLTBI"	= 0.326,
     "TST_pos"	= 0.534,
     "PPV_TST"	= 0.482,
@@ -157,9 +157,9 @@ branch_probs <-
     "QFT_pos"	= 0.281,
     "PPV_QFT"	= 0.928,
     "NPV_QFT"	= 0.909,
-    "TSPOT_pos"	= 0.327,
-    "PPV_TSPOT"	= 0.897,
-    "NPV_TSPOT"	= 0.952,
+    "TSPOT_pos" = 0.327,
+    "PPV_TSPOT" = 0.897,
+    "NPV_TSPOT" = 0.952,
     "QFT_pos_TST+" = 0.401,
     "PPV_QFT_TST+" = 0.961,
     "NPV_QFT_TST+" = 0.839,
@@ -170,15 +170,15 @@ branch_probs <-
     "pComp_chemo" = 0.8,
     "pHep" = 0.002,
     "pReact" = 0.001936869,
-    "pReact_comp"	= 0.000677904,
-    "pReact_incomp"	= 0.001530127
+    "pReact_comp" = 0.000677904,
+    "pReact_incomp" = 0.001530127
   )
 
 hsuv <-
   list(
-    "loss_chemo"	= 0.01,
-    "loss_hep"	= 0.22,
-    "loss_tb"	= 0.15
+    "loss_chemo" = 0.01,
+    "loss_hep" = 0.22,
+    "loss_tb" = 0.15
   )
 
 
