701c27fca6c57ad5a621d19527bf1e678ed2f3ab
Dr Nathan Green <nathan.green@imperial.ac.uk>
2019-01-11 17:03:31 +0000

    add pkgdown yaml webaddress

--- tests/testthat/test-long_to_transmat.R
+++ tests/testthat/test-long_to_transmat.R
@@ -13,7 +13,11 @@ test_that("probability matrices", {
                     to = c(2,3,4,5,6,7),
                     prob = c(0.5,0.5,0.1,0.9,0.3,0.7))
 
+  expect_type(long_to_transmat(dat), "list")
+  expect_is(long_to_transmat(dat), "data.frame")
   expect_true(is_prob_matrix(long_to_transmat(dat)))
+
+  expect_true(all(is.na(diag(as.matrix(long_to_transmat(dat))))), NA)
 
   dat$prob[1] <- 1
   expect_error(assert_that(is_prob_matrix(long_to_transmat(dat))),
